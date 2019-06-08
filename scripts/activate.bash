875445fe850c600c950d8fde6662daf20d91e3e9
ega-wave <health@ega-wave.com>
2019-06-08 16:09:44 +0900

    initial commit

---
+++ scripts/activate.bash
@@ -0,0 +1,158 @@
+
+# description: functions to use tukubai-commands
+# usage: . activate.bash
+
+export TUKUBAI_ROOT=/cygdrive/c/documents/cache/git/ega-wave/Open-usp-Tukubai
+
+1bai () { python3 $TUKUBAI_ROOT/COMMANDS/1bai $*
+}
+
+block_getlast () { python3 $TUKUBAI_ROOT/COMMANDS/block_getlast $*
+}
+calclock () { python3 $TUKUBAI_ROOT/COMMANDS/calclock $*
+}
+calsed () { python3 $TUKUBAI_ROOT/COMMANDS/calsed $*
+}
+cap () { python3 $TUKUBAI_ROOT/COMMANDS/cap $*
+}
+cgi-name () { python3 $TUKUBAI_ROOT/COMMANDS/cgi-name $*
+}
+check_attr_name () { python3 $TUKUBAI_ROOT/COMMANDS/check_attr_name $*
+}
+check_cmp_name () { python3 $TUKUBAI_ROOT/COMMANDS/check_cmp_name $*
+}
+check_date_name () { python3 $TUKUBAI_ROOT/COMMANDS/check_date_name $*
+}
+check_dble_name () { python3 $TUKUBAI_ROOT/COMMANDS/check_dble_name $*
+}
+check_inlist_name () { python3 $TUKUBAI_ROOT/COMMANDS/check_inlist_name $*
+}
+check_need_name () { python3 $TUKUBAI_ROOT/COMMANDS/check_need_name $*
+}
+cjoin0 () { python3 $TUKUBAI_ROOT/COMMANDS/cjoin0 $*
+}
+cjoin1 () { python3 $TUKUBAI_ROOT/COMMANDS/cjoin1 $*
+}
+cjoin1x () { python3 $TUKUBAI_ROOT/COMMANDS/cjoin1x $*
+}
+cjoin2 () { python3 $TUKUBAI_ROOT/COMMANDS/cjoin2 $*
+}
+cjoin2x () { python3 $TUKUBAI_ROOT/COMMANDS/cjoin2x $*
+}
+comma () { python3 $TUKUBAI_ROOT/COMMANDS/comma $*
+}
+count () { python3 $TUKUBAI_ROOT/COMMANDS/count $*
+}
+ctail () { python3 $TUKUBAI_ROOT/COMMANDS/ctail $*
+}
+dayslash () { python3 $TUKUBAI_ROOT/COMMANDS/dayslash $*
+}
+delf () { python3 $TUKUBAI_ROOT/COMMANDS/delf $*
+}
+delr () { python3 $TUKUBAI_ROOT/COMMANDS/delr $*
+}
+divsen () { python3 $TUKUBAI_ROOT/COMMANDS/divsen $*
+}
+exist () { python3 $TUKUBAI_ROOT/COMMANDS/exist $*
+}
+extname () { python3 $TUKUBAI_ROOT/COMMANDS/extname $*
+}
+filehame () { python3 $TUKUBAI_ROOT/COMMANDS/filehame $*
+}
+formhame () { python3 $TUKUBAI_ROOT/COMMANDS/formhame $*
+}
+fsed () { python3 $TUKUBAI_ROOT/COMMANDS/fsed $*
+}
+getfirst () { python3 $TUKUBAI_ROOT/COMMANDS/getfirst $*
+}
+getlast () { python3 $TUKUBAI_ROOT/COMMANDS/getlast $*
+}
+gyo () { python3 $TUKUBAI_ROOT/COMMANDS/gyo $*
+}
+haba () { python3 $TUKUBAI_ROOT/COMMANDS/haba $*
+}
+han () { python3 $TUKUBAI_ROOT/COMMANDS/han $*
+}
+isdate () { python3 $TUKUBAI_ROOT/COMMANDS/isdate $*
+}
+join0 () { python3 $TUKUBAI_ROOT/COMMANDS/join0 $*
+}
+join1 () { python3 $TUKUBAI_ROOT/COMMANDS/join1 $*
+}
+join1x () { python3 $TUKUBAI_ROOT/COMMANDS/join1x $*
+}
+join2 () { python3 $TUKUBAI_ROOT/COMMANDS/join2 $*
+}
+join2x () { python3 $TUKUBAI_ROOT/COMMANDS/join2x $*
+}
+juni () { python3 $TUKUBAI_ROOT/COMMANDS/juni $*
+}
+kasan () { python3 $TUKUBAI_ROOT/COMMANDS/kasan $*
+}
+keta () { python3 $TUKUBAI_ROOT/COMMANDS/keta $*
+}
+keycut () { python3 $TUKUBAI_ROOT/COMMANDS/keycut $*
+}
+loopj () { python3 $TUKUBAI_ROOT/COMMANDS/loopj $*
+}
+loopx () { python3 $TUKUBAI_ROOT/COMMANDS/loopx $*
+}
+maezero () { python3 $TUKUBAI_ROOT/COMMANDS/maezero $*
+}
+man2 () { python3 $TUKUBAI_ROOT/COMMANDS/man2 $*
+}
+map () { python3 $TUKUBAI_ROOT/COMMANDS/map $*
+}
+marume () { python3 $TUKUBAI_ROOT/COMMANDS/marume $*
+}
+mdate () { python3 $TUKUBAI_ROOT/COMMANDS/mdate $*
+}
+mime-read () { python3 $TUKUBAI_ROOT/COMMANDS/mime-read $*
+}
+mojihame () { python3 $TUKUBAI_ROOT/COMMANDS/mojihame $*
+}
+nameread () { python3 $TUKUBAI_ROOT/COMMANDS/nameread $*
+}
+numchar () { python3 $TUKUBAI_ROOT/COMMANDS/numchar $*
+}
+plus () { python3 $TUKUBAI_ROOT/COMMANDS/plus $*
+}
+rank () { python3 $TUKUBAI_ROOT/COMMANDS/rank $*
+}
+ratio () { python3 $TUKUBAI_ROOT/COMMANDS/ratio $*
+}
+retu () { python3 $TUKUBAI_ROOT/COMMANDS/retu $*
+}
+self () { python3 $TUKUBAI_ROOT/COMMANDS/self $*
+}
+selr () { python3 $TUKUBAI_ROOT/COMMANDS/selr $*
+}
+sm2 () { python3 $TUKUBAI_ROOT/COMMANDS/sm2 $*
+}
+sm4 () { python3 $TUKUBAI_ROOT/COMMANDS/sm4 $*
+}
+sm5 () { python3 $TUKUBAI_ROOT/COMMANDS/sm5 $*
+}
+tarr () { python3 $TUKUBAI_ROOT/COMMANDS/tarr $*
+}
+tateyoko () { python3 $TUKUBAI_ROOT/COMMANDS/tateyoko $*
+}
+tcat () { python3 $TUKUBAI_ROOT/COMMANDS/tcat $*
+}
+ulock () { python3 $TUKUBAI_ROOT/COMMANDS/ulock $*
+}
+unmap () { python3 $TUKUBAI_ROOT/COMMANDS/unmap $*
+}
+up3 () { python3 $TUKUBAI_ROOT/COMMANDS/up3 $*
+}
+yarr () { python3 $TUKUBAI_ROOT/COMMANDS/yarr $*
+}
+ycat () { python3 $TUKUBAI_ROOT/COMMANDS/ycat $*
+}
+yobi () { python3 $TUKUBAI_ROOT/COMMANDS/yobi $*
+}
+ysum () { python3 $TUKUBAI_ROOT/COMMANDS/ysum $*
+}
+zen () { python3 $TUKUBAI_ROOT/COMMANDS/zen $*
+}
+
